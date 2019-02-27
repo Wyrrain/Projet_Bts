d75372b797ae5b92b4daa97422d8c3cf8055a0dd
John Doe <pierregorge55@gmail.com>
2019-02-27 12:09:35 +0100

    dg

---
+++ Android_Studio/Pierre/QRCAMERA/app/src/main/java/com/example/scanner_qr/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scanner_qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
