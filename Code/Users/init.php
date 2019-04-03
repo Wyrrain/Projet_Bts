2ecaa42f43a95058a8f25d7ed774afcc78f1b183
Smort <cesar_reyes97@hotmail.com>
2019-04-03 10:18:19 +0200

    ds

---
+++ Code/Users/init.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$host = "10.16.37.120";
+$user_name = "root";
+$user_password = "Aqwzsx75386";
+$db_name = "bdd_rfid";
+
+$con = mysqli_connect($host, $login, $motdepasse, $db_name);
+
+if($con)
+echo "Connection success..."
+else
+echo "Connection Failed..."
+
+ 
+
+
+?>
