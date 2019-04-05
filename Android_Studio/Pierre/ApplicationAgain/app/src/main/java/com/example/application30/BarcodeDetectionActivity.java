3a2ec1f0677a092f59d0f3a34871066a93399314
John Doe <pierregorge55@gmail.com>
2019-04-05 12:08:57 +0200

    25

---
+++ Android_Studio/Pierre/ApplicationAgain/app/src/main/java/com/example/application30/BarcodeDetectionActivity.java
@@ -0,0 +1,59 @@
+/*package com.example.application30;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class BarcodeDetectionActivity extends AppCompatActivity {
+
+    BarcodeDetector barcodeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcodescanner);
+
+            barcodeDetector = new BarcodeDetector.Builder(this)
+                    .setBarcodeFormats(Barcode.QR_CODE);
+
+            return;
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if (qrCodes.size() != 0) {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+// Vibrate for 500 milliseconds
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                v.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+                            }
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+
+}*/
